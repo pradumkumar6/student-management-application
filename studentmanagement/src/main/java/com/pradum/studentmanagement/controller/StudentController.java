6ad9b8cff44c3edabd421c9e1d28539bc232e09a
pradumkumar6 <pradumjaiswar790@gmail.com>
2024-05-30 17:48:25 +0530

    Implementing dao layer

--- studentmanagement/src/main/java/com/pradum/studentmanagement/controller/StudentController.java
+++ studentmanagement/src/main/java/com/pradum/studentmanagement/controller/StudentController.java
@@ -1,9 +1,7 @@
 package com.pradum.studentmanagement.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -17,7 +15,30 @@ public class StudentController {
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
-
+    @PostMapping
+    public Student save(
+            @RequestBody Student student
+    ){
+        return  studentService.save(student);
+    }
+    @GetMapping("/{email}")
+    public Student findByEmail(
+            @PathVariable("email") String email
+    ){
+        return  studentService.findByEmail(email);
+    }
+    @PutMapping
+    public Student updateStudent(
+           @RequestBody Student student
+    ){
+        return  studentService.update(student);
+    }
+    @DeleteMapping("/{email}")
+    public void delete(
+           @PathVariable("email") String email
+    ){
+        studentService.delete(email);
+    }
     @GetMapping
     public List<Student> getAllStudents(){
         return studentService.getAllStudents();
--- studentmanagement/src/main/java/com/pradum/studentmanagement/controller/StudentService.java
+++ studentmanagement/src/main/java/com/pradum/studentmanagement/controller/StudentService.java
@@ -1,27 +1,13 @@
 package com.pradum.studentmanagement.controller;
 
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
-@Service
-public class StudentService {
-    public List<Student> getAllStudents(){
-        return List.of(
-                new Student(
-                        "Pradum",
-                        "Kumar",
-                        "pradumjaiswar790@gmail.com",
-                        23
-                ),
-                new Student(
-                        "Manish",
-                        "Kumar",
-                        "manishkumar790@gmail.com",
-                        23
-                )
-        );
 
-    }
+public interface StudentService {
+    Student save(Student s);
+    List<Student> getAllStudents();
+    Student findByEmail(String email);
+    Student update(Student s);
+    void delete(String email);
 
 }
